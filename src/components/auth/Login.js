82eeaad22352c7e239d53cc9be206df711c3f734
Aristotle Poultney <aristotlepoultney@gmail.com>
2018-12-31 02:07:27 -0500

    added expandable notification for invalid logins

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-// import { compose } from 'redux';
-// import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import { firebaseConnect } from 'react-redux-firebase';
+import { notifyUser } from '../../actions/notifyActions';
+import Alert from '../layout/Alert';
 
 class Login extends Component {
   state = {
@@ -16,7 +18,7 @@ class Login extends Component {
   onSubmit = e => {
     e.preventDefault();
 
-    const { firebase, history } = this.props;
+    const { firebase, history, notifyUser } = this.props;
     const { email, password } = this.state;
 
     firebase
@@ -24,15 +26,20 @@ class Login extends Component {
         email,
         password
       })
-      .catch(err => alert('Invalid Login Credentials'));
+      .catch(err => notifyUser('Invalid Login Credentials', 'error'));
   };
 
   render() {
+    const { message, messageType } = this.props.notify;
+
     return (
       <div className="row">
         <div className="col-md-6 mx-auto">
           <div className="card">
             <div className="card-body">
+              {message ? (
+                <Alert message={message} messageType={messageType} />
+              ) : null}
               <h1 className="text-center pb-4 pt-3">
                 <span className="text-primary">
                   <i className="fas fa-lock" /> Login
@@ -79,4 +86,12 @@ Login.propTypes = {
   firebase: PropTypes.object.isRequired
 };
 
-export default firebaseConnect()(Login);
+export default compose(
+  firebaseConnect(),
+  connect(
+    (state, props) => ({
+      notify: state.notify
+    }),
+    { notifyUser }
+  )
+)(Login);
